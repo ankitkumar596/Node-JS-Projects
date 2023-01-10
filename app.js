4a82b1e0d41494ac3b9567120bcaee06ce6c0bbe
Unthinkable-Lap <unthinkable-lap@PG02R1TR>
2023-01-10 12:29:14 +0530

    added

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port https://localhost ${port}`)
+  console.log(`Example app listening on port https://localhost:3000 ${port}`)
 })
